77e069e94ab5ff613462ed891d48745388ae7f99
Dawson Le <97484417+kledawson@users.noreply.github.com>
2024-02-26 02:32:21 -0800

    Add files via upload

---
+++ 8_binaryliterals.py
@@ -0,0 +1,25 @@
+'''
+Dawson Le
+CIS 41A   Winter 2023
+Unit C, Problem C
+'''
+
+a = 9
+b = 14
+
+print("binary of a = ", bin(a))
+
+print("binary of b = ", bin(b))
+
+print("binary of a & b = ", bin(a & b))
+
+print("binary of a | b = ", bin(a | b))
+
+'''Example output:
+binary of a =  0b1001
+binary of b =  0b1110
+binary of a & b =  0b1000
+binary of a | b =  0b1111
+'''
+
+print("end of program.")
