3a1f995c0f1ce044195989e607eee44388f84087
Dawson Le <97484417+kledawson@users.noreply.github.com>
2024-02-26 02:20:21 -0800

    Add files via upload

---
+++ 2_areaexercise.py
@@ -0,0 +1,33 @@
+'''
+Dawson Le
+CIS 41A   Fall 2021
+Unit A, Exercise A
+'''
+
+height = 2.9
+width = 7.1
+
+area=height*width
+
+print("height: ", height)
+print("width: ", width)
+print("area: ", area)
+
+
+height = height//2
+width = width//2
+
+area = height * width
+print("height: ", height)
+print("width: ", width)
+print("area: ", area)   
+
+
+'''
+height:  2.9
+width:  7.1
+area:  20.59
+height:  1.0
+width:  3.0
+area:  3.0
+'''
